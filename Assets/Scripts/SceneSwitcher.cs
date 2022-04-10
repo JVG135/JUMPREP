874531ab787ca9466f22427d205f8963466782ff
JVGLAPTOP\Johannes Vince Gomez <johannes.gomez@ciit.edu.ph>
2022-04-11 04:04:42 +0800

    save game state script added

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
-{   
+{
+
+    public void NextScene(string level)
+    {
+        SceneManager.LoadScene(level); //load scene specified in editor
+    }
+    
     public void Exit()
     {
         Application.Quit(); //close game (works in final build, but not editor?)
     }
+
 }
